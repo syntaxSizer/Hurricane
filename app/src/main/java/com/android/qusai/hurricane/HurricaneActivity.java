6be9d97fef725f00cd6e040188e2d04ecd425398
syntaxsizer <kakuma4never@gmail.com>
2016-05-28 22:29:12 +0300

    add project gradle files

---
+++ app/src/main/java/com/android/qusai/hurricane/HurricaneActivity.java
@@ -0,0 +1,13 @@
+package com.android.qusai.hurricane;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HurricaneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hurricane);
+    }
+}
