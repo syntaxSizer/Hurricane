1c55e0cbeb48af3ff2efed20e44de1f0ed933726
syntaxsizer <kakuma4never@gmail.com>
2016-05-28 22:50:54 +0300

    add api client id

---
+++ app/src/main/java/com/android/qusai/hurricane/Config.java
@@ -0,0 +1,9 @@
+package com.android.qusai.hurricane;
+
+/**
+ * Created by qusai on 5/28/16.
+ */
+public class Config {
+    public static final String CLIENT_ID="19f3a5711dae0dbfdf123ff366cf2d5c";
+    public static final String API_URL="https://api.soundcloud.com";
+}
