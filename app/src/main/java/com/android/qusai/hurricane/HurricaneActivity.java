066beb98f1538ac5ee540e564fc4dbf7b0dad921
syntaxsizer <kakuma4never@gmail.com>
2016-05-29 15:27:02 +0300

    add tracks fetched from the api to the view arraylist

--- app/src/main/java/com/android/qusai/hurricane/HurricaneActivity.java
+++ app/src/main/java/com/android/qusai/hurricane/HurricaneActivity.java
@@ -3,9 +3,11 @@ package com.android.qusai.hurricane;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
 
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -15,6 +17,8 @@ import retrofit.RetrofitError;
 import retrofit.client.Response;
 
 public class HurricaneActivity extends Activity {
+    private List<Track> mListItems;
+    private SCTrackAdapter mAdapter;
     private static final String TAG = "HurricaneActivity";
 
     @Override
@@ -22,16 +26,22 @@ public class HurricaneActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hurricane);
 
+        mListItems = new ArrayList<Track>();
+        ListView listView = (ListView)findViewById(R.id.track_list_view);
+        mAdapter = new SCTrackAdapter(this,mListItems);
+        listView.setAdapter(mAdapter);
+
 //        Log.e("stuff","Stuff");
         RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(Config.API_URL).build();
         SCService scService = restAdapter.create(SCService.class);
         scService.getRecentTracks(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()), new Callback<List<Track>>() {
             @Override
             public void success(List<Track> tracks, Response response) {
-//                Log.e("stuff2",tracks.toString());
-                if (tracks == null) {
-                    Log.e(TAG, "First track title: " + tracks.get(0).getTitle());
-                }
+//
+////                if (tracks == null) {
+////                    Log.e(TAG, "First track title: " + tracks.get(0).getTitle());
+//                }
+                loadTracks(tracks);
 
             }
 
@@ -40,5 +50,12 @@ public class HurricaneActivity extends Activity {
                 Log.e(TAG, "Error: " + error);
             }
         });
+    }
+
+    private  void loadTracks(List<Track> tracks){
+        mListItems.clear();
+        mListItems.addAll(tracks);
+        mAdapter.notifyDataSetChanged();
+
     }
 }
